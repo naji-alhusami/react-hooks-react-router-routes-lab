2ca6c45f96fb5301ab0aff3fdd5dea2377d6684c
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-12-10 21:31:59 +0300

    Completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { directors } from "../data";
 
+const showDirectorsData = (director) => {
+  return (
+    <div key={director.name}>
+      <h4>{director.name}</h4>
+      <ul>
+        {director.movies.map((movie) => {
+          return <li  key={movie}>{movie}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => showDirectorsData(director))}
+    </div>
+  );
 }
 
 export default Directors;
